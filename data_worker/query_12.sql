672cb780359e16bbc3a22f3fcd08cc2a1015124d
MykolaMS9 <139469635+MykolaMS9@users.noreply.github.com>
2023-08-24 22:28:23 +0300

    v_0.1.1

--- data_worker/query_12.sql
+++ data_worker/query_12.sql
@@ -1,7 +1,9 @@
-SELECT s.fullname as student_name, gr.name, d.name as discipline, g.grade, g.date_of 
+SELECT GROUP_CONCAT(s.fullname) as student_name, gr.name, d.name as discipline, GROUP_CONCAT(g.grade), g.date_of 
 FROM disciplines d
 JOIN grades g ON g.discipline_id = d.id  
 JOIN students s ON s.id = g.student_id 
 JOIN [groups] gr ON gr.id = s.group_id 
-WHERE gr.id = 5 AND d.id = 1
-ORDER BY g.date_of  DESC;
+WHERE gr.id = 1 AND d.id = 1
+GROUP BY g.date_of 
+ORDER BY g.date_of  DESC
+LIMIT 1;
